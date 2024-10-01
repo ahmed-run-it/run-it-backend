1a382da2ce786a825153d518642a4094fa951d59
ahmed-run-it <ahmed.benothmen.avec@gmail.com>
2024-10-01 13:32:30 +0100

    added prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,9 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -21,5 +23,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
   },
 };
